6e49acebff3992dc2e20d0a670af61b0c84ddfda
attevinon <violet.lutsevich@gmail.com>
2022-05-09 14:09:52 +0300

    add font icons

--- MyDbtDiaryCard/MyDbtDiaryCard/AssemblyInfo.cs
+++ MyDbtDiaryCard/MyDbtDiaryCard/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)] 
+
+[assembly: ExportFont("Font Awesome 6 Free-Solid-900.otf", Alias = "FAS")]
+[assembly: ExportFont("Font Awesome 6 Free-Regular-400.otf", Alias = "FAR")]
