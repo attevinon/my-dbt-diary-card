e5c2ec7b68d3b185beed988241dcf0f584549965
attevinon <violet.lutsevich@gmail.com>
2022-05-09 14:11:48 +0300

    set culture for tests

--- MyDbtDiaryCard/MyDbtDiaryCard/App.xaml.cs
+++ MyDbtDiaryCard/MyDbtDiaryCard/App.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using MyDbtDiaryCard.Views;
-using Xamarin.Forms;
-using Prism.Mvvm;
-using Xamarin.Forms.Xaml;
 using MyDbtDiaryCard.Services.Navigation;
 using MyDbtDiaryCard.Services.DataService;
 using System.IO;
+using System.Globalization;
 
 namespace MyDbtDiaryCard
 {
@@ -14,21 +12,31 @@ namespace MyDbtDiaryCard
         public static INavigationService NavigationService { get; } = new PageNavigationService();
         public App()
         {
-            InitializeComponent();
+            CultureInfo.CurrentUICulture = new CultureInfo("ru");
 
-            /*File.Delete(Path.Combine(Environment.GetFolderPath(
-                        Environment.SpecialFolder.LocalApplicationData), "MyDbtDiaryDatabase.db3"));*/
+            //File.Delete(Path.Combine(Environment.GetFolderPath(
+            //Environment.SpecialFolder.LocalApplicationData), "MyDbtDiaryDb.db3"));
+
+            InitializeComponent();
 
             DataService.GetDataManager().InitializeAsync(
                 Path.Combine(Environment.GetFolderPath(
-                        Environment.SpecialFolder.LocalApplicationData),"MyDbtDiaryDb.db3"));
+                        Environment.SpecialFolder.LocalApplicationData), "MyDbtDiaryDb.db3")); ;
 
             NavigationService.Configure(nameof(DayEntryPage), typeof(DayEntryPage));
             NavigationService.Configure(nameof(AddDayEntryPage), typeof(AddDayEntryPage));
             NavigationService.Configure(nameof(OverviewPage), typeof(OverviewPage));
+            NavigationService.Configure(nameof(DbtSkillsPage), typeof(DbtSkillsPage));
             NavigationService.Configure(nameof(Views.MainPage), typeof(Views.MainPage));
 
-            MainPage = (NavigationService as PageNavigationService)?.SetRootPage(nameof(Views.MainPage));
+            try
+            {
+                MainPage = (NavigationService as PageNavigationService)?.SetRootPage(nameof(Views.MainPage));
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
         }
 
         protected override void OnStart()
