e415e2b7d4e8ecb4c53f315f249e2633d8726a89
attevinon <violet.lutsevich@gmail.com>
2022-05-16 16:17:56 +0300

    add date converter

---
+++ MyDbtDiaryCard/MyDbtDiaryCard/Converters/DateTimeConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyDbtDiaryCard.Converters
+{
+    internal class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is DateTime))
+                return string.Empty;
+
+            if (bool.Parse(value as string ?? "False"))
+                return (culture == CultureInfo.GetCultureInfo("ru")) ? $"{value:ddd}, {value:D}" : $"{value:D}";
+
+            return $"{value:ddd}, {value:d}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
