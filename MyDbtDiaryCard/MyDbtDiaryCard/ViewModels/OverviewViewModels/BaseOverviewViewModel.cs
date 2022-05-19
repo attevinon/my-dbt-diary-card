b4478fab736af4466f3cbc54f95967daf11a3ed9
attevinon <violet.lutsevich@gmail.com>
2022-05-19 17:33:34 +0300

    add BaseOverviewVM for common properties of TableVM and OverviewVM

---
+++ MyDbtDiaryCard/MyDbtDiaryCard/ViewModels/OverviewViewModels/BaseOverviewViewModel.cs
@@ -0,0 +1,83 @@
+﻿using MyDbtDiaryCard.Services.Commands;
+using MyDbtDiaryCard.Services.Navigation;
+using System;
+using System.Windows.Input;
+
+namespace MyDbtDiaryCard.ViewModels
+{
+    internal class BaseOverviewViewModel : BaseViewModel
+    {
+        private bool isEnoughEntries;
+        public bool IsEnoughEntries
+        {
+            get { return isEnoughEntries; }
+            set { SetProperty(ref isEnoughEntries, value); }
+        }
+
+        private int daysRange = 7;
+        public virtual int DaysRange
+        {
+            get { return daysRange; }
+            set
+            {
+                int oldValue = daysRange;
+                SetProperty(ref daysRange, value);
+                EndDate = EndDate.AddDays(value - oldValue);
+            }
+        }
+
+        private DateTime startDate;
+        public virtual DateTime StartDate
+        {
+            get => startDate;
+            set
+            {
+                if (value > DateTime.Today)
+                    throw new Exception("Start date must not be from future");
+
+                SetProperty(ref startDate, value);
+                EndDate = value.AddDays(DaysRange);
+
+            }
+        }
+
+        private DateTime endDate;
+        public virtual DateTime EndDate
+        {
+            get => endDate;
+            set
+            {
+                if (value > DateTime.Today)
+                    value = DateTime.Today;
+
+                SetProperty(ref endDate, value);
+            }
+        }
+
+        public ICommand GetPreviousRangeCommand { get; }
+        public ICommand GetNextRangeCommand { get; }
+
+        public BaseOverviewViewModel(INavigationService navigation) : base(navigation)
+        {
+            StartDate = DateTime.Today.AddDays(-DaysRange);
+
+            GetNextRangeCommand = new DateCommand(() => StartDate = StartDate.AddDays(DaysRange));
+            GetPreviousRangeCommand = new ActionCommand(GetPreviousRange);
+        }
+
+        private void GetPreviousRange()
+        {
+            var date = StartDate.Ticks;
+            var days = TimeSpan.FromDays(DaysRange).Ticks;
+
+            if (date - days < DateTime.MinValue.Ticks)
+            {
+                StartDate = DateTime.MinValue;
+                return;
+            }
+
+            StartDate = StartDate.AddDays(-DaysRange);
+
+        }
+    }
+}
---
+++ MyDbtDiaryCard/MyDbtDiaryCard/ViewModels/OverviewViewModels/TableViewModel.cs
@@ -0,0 +1,76 @@
+﻿using MyDbtDiaryCard.Services.Navigation;
+using MyDbtDiaryCard.Services.DataService;
+using System;
+using System.Collections.Generic;
+using MyDbtDiaryCard.Model;
+using System.Windows.Input;
+using MyDbtDiaryCard.Services.Commands;
+
+namespace MyDbtDiaryCard.ViewModels
+{
+    internal class TableViewModel : BaseOverviewViewModel
+    {
+        public override int DaysRange
+        {
+            get { return base.DaysRange; }
+            set 
+            {
+                base.DaysRange = value;
+            }
+        }
+
+        public override DateTime EndDate
+        {   
+            get => base.EndDate;
+            set
+            {
+                base.EndDate = value;
+                RefreshTable();
+            }
+        }
+
+        private IEnumerable<DayEntry> entries;
+        public IEnumerable<DayEntry> Entries
+        {
+            get => entries;
+            set {SetProperty(ref entries, value); } 
+        }
+
+        public ICommand GoBackCommand { get; }
+
+        public TableViewModel(INavigationService navigation) : base(navigation)
+        {
+            GoBackCommand = new ActionCommand(async () => await NavigationService.GoBackAsync());
+        }
+
+        private async void RefreshTable()
+        {
+            try
+            {
+                IsLoading = true;
+
+                if (DataService.GetDataManager().DayEntryData.HasBeenInitialized == false)
+                    return;
+
+                var entries = new Model.EntriesStats.BaseEntriesStats();
+                await entries.Initialize(StartDate, EndDate);
+
+                if (entries.DaysCount < 1)
+                {
+                    IsEnoughEntries = false;
+                    IsLoading = false;
+                    return;
+                }
+
+                IsEnoughEntries = true;
+                Entries = new List<DayEntry>(entries.DaysOfPeriod);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex.StackTrace);
+            }
+
+            IsLoading = false;
+        }
+    }
+}
