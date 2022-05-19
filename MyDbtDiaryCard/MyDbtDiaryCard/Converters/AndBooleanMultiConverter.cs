23e087ed0c28ee2b8a9b48ae5b7d87a69e3330db
attevinon <violet.lutsevich@gmail.com>
2022-05-19 19:12:02 +0300

    add multiconverter for bool

---
+++ MyDbtDiaryCard/MyDbtDiaryCard/Converters/AndBooleanMultiConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MyDbtDiaryCard.Converters
+{
+    internal class AndBooleanMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = true;
+
+            foreach (var value in values)
+            {
+                if (value is bool boolean)
+                {
+                    if (!boolean)
+                        return false;
+                }
+            }
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
